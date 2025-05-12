88aad83ec82d240e13a34958e97c682fac5d9865
GianCestarolli <giancestarolli@gmail.com>
2025-05-12 10:58:33 -0300

    Day5: Fixing the type of variables.

--- 5/src/main/java/BudgetTracker.java
+++ 5/src/main/java/BudgetTracker.java
@@ -4,27 +4,27 @@ public class BudgetTracker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Asking for all the information
         System.out.print("Enter your monthly income: ");
-        int Income = scanner.nextInt();
-        scanner.nextLine();// Consumes the pending line.
+        double Income = scanner.nextDouble();
 
         System.out.print("Enter the value of your rent: ");
-        int Rent = scanner.nextInt();
-        scanner.nextLine();
+        double Rent = scanner.nextDouble();
 
         System.out.print("Enter your monthly groceries expense: ");
-        int Groceries = scanner.nextInt();
-        scanner.nextLine();
+        double Groceries = scanner.nextDouble();
 
         System.out.print("Enter your monthly transportation expense: ");
-        int Transportation = scanner.nextInt();
-        scanner.nextLine();
+        double Transportation = scanner.nextDouble();
 
         System.out.print("Enter your monthly entertainment expense: ");
-        int Entmt = scanner.nextInt();
-        scanner.nextLine();
+        double Entmt = scanner.nextDouble();
 
+        //Calculating
+        double TotalExpenses = (Rent + Groceries + Transportation + Entmt);
+        double RemainingBudget = (Income - TotalExpenses);
 
-
+        double RentP = ((Rent/Income) * 100);
+        System.out.println(RentP);
     }
 }
