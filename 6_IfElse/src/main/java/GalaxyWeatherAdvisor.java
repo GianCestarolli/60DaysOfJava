89537d12e50bea86180fdaf7686adb002f7ff592
GianCestarolli <giancestarolli@gmail.com>
2025-05-13 11:36:33 -0300

    Day6: Starting the Galaxy weather advisor.

---
+++ 6_IfElse/src/main/java/GalaxyWeatherAdvisor.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class GalaxyWeatherAdvisor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Galaxy Weather Advisor!");
+
+        System.out.print("Tell me the planet you are on (Earth, Mars, Venus or Jupiter): ");
+        String planet = scanner.nextLine().toLowerCase();
+
+
+
+    }
+}
