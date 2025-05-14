4bab113b9414ba5f731398d82645e78e99390128
GianCestarolli <giancestarolli@gmail.com>
2025-05-14 09:24:15 -0300

    Day6: Creating the first 'IFs'

--- 6_IfElse/src/main/java/GalaxyWeatherAdvisor.java
+++ 6_IfElse/src/main/java/GalaxyWeatherAdvisor.java
@@ -10,7 +10,14 @@ public class GalaxyWeatherAdvisor {
         System.out.print("Tell me the planet you are on (Earth, Mars, Venus or Jupiter): ");
         String planet = scanner.nextLine().toLowerCase();
 
+        if (planet.equals("mars") || planet.equals("earth") || planet.equals("venus") || planet.equals("jupiter")) {
+            System.out.print("What's the current temperature on" + planet + ",in Celsius: ");
+            int temperature = scanner.nextInt();
+            scanner.nextLine();
 
-
+            if (temperature < 20) {
+                System.out.println("Wow, it's very cold ! You should wear a jacket and pants.");
+            }
+        }
     }
 }
