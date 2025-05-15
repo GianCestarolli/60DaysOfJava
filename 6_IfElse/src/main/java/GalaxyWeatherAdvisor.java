77c541b4c5d3483406005d7057f01a4242396e7a
GianCestarolli <giancestarolli@gmail.com>
2025-05-15 09:24:19 -0300

    Day6: GWA finished

--- 6_IfElse/src/main/java/GalaxyWeatherAdvisor.java
+++ 6_IfElse/src/main/java/GalaxyWeatherAdvisor.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class GalaxyWeatherAdvisor {
@@ -11,13 +10,20 @@ public class GalaxyWeatherAdvisor {
         String planet = scanner.nextLine().toLowerCase();
 
         if (planet.equals("mars") || planet.equals("earth") || planet.equals("venus") || planet.equals("jupiter")) {
-            System.out.print("What's the current temperature on" + planet + ",in Celsius: ");
+            System.out.print("What's the current temperature on " + planet + ", in Celsius: ");
             int temperature = scanner.nextInt();
             scanner.nextLine();
 
             if (temperature < 20) {
                 System.out.println("Wow, it's very cold ! You should wear a jacket and pants.");
+            } else if (temperature > 100) {
+                System.out.println("OMG! It's like an oven!!!! You're gonna die!!!");
+            } else {
+                System.out.println("It's hot, but not that hot... You can wear shorts and a shirt");
             }
+        }
+        else {
+            System.out.println("Sorry, " + planet + " is not a valid planet.");
         }
     }
 }
