985850614ea914f56f82a5f30c19f2999c96105f
GianCestarolli <giancestarolli@gmail.com>
2025-05-19 09:09:31 -0300

    Day7: Adventure Game finished.

--- 7/src/main/java/AdventureGame.java
+++ 7/src/main/java/AdventureGame.java
@@ -12,15 +12,32 @@ public class AdventureGame {
                 "The sun is rising. Today is the day that you're planning to escape.\n" +
                 "The guard opens your cage, he is escorting you too the laundry.\n" +
                 "Now you are in a dark corridor, with no cameras.\n" +
-                "Do want to punch him and take his weapon or wait till you reach the laundry?\n" +
+                "Do you want to punch him and take his weapon or wait until you reach the laundry?\n" +
                 "Type 'Fight' or 'Wait': ");
-                String FirstAnswer = scanner.nextLine().toLowerCase();
+        String FirstAnswer = scanner.nextLine().toLowerCase();
 
         if (FirstAnswer.equals("fight")) {
             System.out.println("The guard was alert.\n" +
                     "He takes the gun and shoots you.\n" +
                     "Game Over!");
-        }
+        } else if (FirstAnswer.equals("wait")) {
+            System.out.print("You wait until you reach the laundry." +
+                    "After that, the guard leaves you with the others, there's more 40 prisoners washing the clothes.\n" +
+                    "There's nobody looking at you.\n" +
+                    "Do you want to keep washing the clothes or sneak to the ventilation ducts?\n" +
+                    "Type 'Keep' or 'Sneak'");
+            String SecondAnswer = scanner.nextLine().toLowerCase();
 
+            if (SecondAnswer.equals("keep")) {
+                System.out.println("You keep washing the clothes for the rest of the day.\n" +
+                        "Game Over!");
+            } else if (SecondAnswer.equals("sneak")) {
+                System.out.println("You did it. now you're in the ventilation ducts...\n" +
+                        "The games continues...");
+            }
+        }else {
+            System.out.println("That's not a valid answer");
+        }
     }
 }
+
