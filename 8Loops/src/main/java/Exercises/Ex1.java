1d2e1999966dc722a6935d277bce057fb84ab11c
GianCestarolli <giancestarolli@gmail.com>
2025-05-20 11:39:47 -0300

    Day8: Practicing with exercise1

---
+++ 8Loops/src/main/java/Exercises/Ex1.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+//Even numbers from 1 to 100
+public class Ex1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100 ; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
